658c0a94d4f30658a10bc30a4c562c388b9c3caa
barrylee.li <barry.ppz@gamil.com>
2019-12-23 00:11:51 +0800

    springboot demo7 - mybatis init

---
+++ springbootdemo7/src/test/java/com/bl/springbootdemo7/springbootdemo7/Springbootdemo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.springbootdemo7.springbootdemo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo7ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
